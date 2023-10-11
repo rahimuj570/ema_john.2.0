1205161353207a0985ae4495ccb5f962b61b606a
rahimuj570 <rahimuj570@gmail.com>
2023-10-11 15:29:43 +0600

    repo created

---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+const Products = () => {
+  return (
+    <div>
+      <h1>hellllllo</h1>
+    </div>
+  );
+};
+
+export default Products;
