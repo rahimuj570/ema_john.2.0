8a4602839941894d050cdb5358abd1a7fea693e9
rahimuj570 <rahimuj570@gmail.com>
2023-10-15 14:37:07 +0600

    successfully fetch data from fakeData

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,16 +1,26 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
+import { useEffect } from "react";
 import SidebarCart from "../Carts/SidebarCart";
 import "./Products.css";
 import SingleProduct from "./SingleProduct";
+import fakeData from "../../fakeData/products.json";
+import { useState } from "react";
 
 const Products = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    setData(fakeData);
+    console.log(data);
+  }, [data]);
   return (
     <div className="flex justify-between">
-      <div className="flex justify-center mx-auto gap-11 h-fit mt-20">
-        <SingleProduct />
-        <SingleProduct />
-        <SingleProduct />
+      <div className="grid grid-cols-3 mx-auto gap-11 h-fit mt-20">
+        {data.map((e) => (
+          <SingleProduct key={e.id} data={e} />
+        ))}
+        {/* <SingleProduct />
+        <SingleProduct /> */}
       </div>
       <SidebarCart />
     </div>
--- src/components/Products/SingleProduct.jsx
+++ src/components/Products/SingleProduct.jsx
@@ -1,29 +1,35 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import React, { Component } from "react";
 
-class SingleProduct extends Component {
-  render() {
-    return (
-      <div className="shadow-lg text-center border-gray-300 border rounded py-10 pb-0  pt-0">
+const SingleProduct = ({ data }) => {
+  console.log(data);
+  return (
+    <div className="flex flex-col justify-between w-56 shadow-lg text-center border-gray-300 border rounded py-10 pb-0  pt-0">
+      <div className="">
         <div className="m-1 rounded bg-slate-200">
           <img
             className="hover:scale-105 duration-200 py-5 w-4/6 inline-block"
-            src="chrome://branding/content/about-logo.png"
+            src={
+              data.img == null
+                ? "chrome://branding/content/about-logo.png"
+                : data.img
+            }
             alt=""
           />
         </div>
-        <h2 className="mt-2 font-bold text-lg">Title</h2>
+        <h2 className="mt-2 font-bold text-lg">{data.name}</h2>
         <p className="product_price"></p>
         <div className="px-1 text-start text-sm mt-2">
-          <p>Manufacture : Address</p>
-          <p>Rating : 3star</p>
+          <p>Manufacture : {data.seller}</p>
+          <p>Rating : {data.ratings} star</p>
         </div>
-        <button className="hover:bg-orange-300 duration-200 mt-2 py-2 w-full bg-orange-200">
-          Add to Cart
-        </button>
       </div>
-    );
-  }
-}
+      <button className="hover:bg-orange-300 duration-200 mt-2 py-2 w-full bg-orange-200">
+        Add to Cart
+      </button>
+    </div>
+  );
+};
 
 export default SingleProduct;
